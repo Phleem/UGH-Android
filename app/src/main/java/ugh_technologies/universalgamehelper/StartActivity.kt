39f1c117050b1d1394774acb028346299e9a169a
Unknown <ThousandPies@web.de>
2018-11-15 14:21:01 +0100

    added configuration xml file to load nav drawer from

--- app/src/main/java/ugh_technologies/universalgamehelper/StartActivity.kt
+++ app/src/main/java/ugh_technologies/universalgamehelper/StartActivity.kt
@@ -1,34 +1,35 @@
 package ugh_technologies.universalgamehelper
 
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
-import android.view.Gravity
-import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        val res: Resources = resources
 
-        setupNavDrawer()
+        val config = res.getStringArray(R.array.configuration)
+
+        setupNavDrawer(config)
 
 
     }
 
-    private fun setupNavDrawer() {
-        val item1:PrimaryDrawerItem = PrimaryDrawerItem().withName("Test1")
-        val item2:SecondaryDrawerItem = SecondaryDrawerItem().withName("Test2")
+    private fun setupNavDrawer(configuration:Array<String>) {
         val drawer = DrawerBuilder()
                 .withActivity(this)
                 .withTranslucentStatusBar(false)
-                .addDrawerItems(item1, item2)
                 .withFullscreen(false)
                 .build()
-
+        for (feature in configuration) {
+            var item = PrimaryDrawerItem().withName(feature)
+            drawer.addItem(item)
+        }
     }
 }
